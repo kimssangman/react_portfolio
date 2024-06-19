25b1f2fe502aec4ffdbdbc05d4ae024834068db5
kimssangman <136281131+kimssangman@users.noreply.github.com>
2024-06-19 15:05:51 +0900

    about 완료

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,21 +4,33 @@ function About() {
     return (
         <div className="about_container">
             <div className="about_title">
-                <h1 className="heading">About Me</h1>
+                <h1 className="heading">
+                    About Me
+                    <span className="highlight"></span>
+                </h1>
             </div>
             <div className="about_content">
-                <h1 className="about_content_title">노력하는 사람</h1>
+                <h1 className="about_content_title">Frontend Engineer</h1>
                 <section className="about_section">
-                    <p>
-                        Lorem ipsum dolor, sit amet consectetur adipisicing
-                        elit.
+                    <span>
+                        2년 차 웹 개발자로 Next.js와 Angular2, React, AWS,
+                        Github를 이용하여 프로젝트 설계, 배포, 운영까지 해온
+                        경험을 가지고 있습니다.
                         <br />
-                        Officia distinctio ipsam facere aperiam non, fuga
-                        suscipit nihil eveniet quod recusandae omnis. Facere
-                        enim sunt magnam.
                         <br />
-                        Voluptatem autem harum tempore consequuntur!
-                    </p>
+                        <br />
+                        사용자 경험을 최우선으로 생각하며 개발하고, 항상 더 높은
+                        목표를 위해 꾸준히 노력하는 개발자 김상민입니다.
+                        <br />
+                        <br />
+                        <br />
+                        계속해서 완성도 높은 코드, 그리고 완성도 높은 UI를 위해
+                        매일 고뇌하는 사람이 되도록 하겠습니다.
+                        <br />
+                        <br />
+                        <br />
+                        감사합니다.
+                    </span>
                 </section>
             </div>
         </div>
--- src/hooks/useScrollNavigation.js
+++ src/hooks/useScrollNavigation.js
@@ -11,7 +11,12 @@ function useScrollNavigation() {
         currentPage: 0,
     });
 
-    // 페이지 로드 시 한 번 실행되는 useEffect
+    /**------------------------------------------------
+     * 페이지 로드 시 한 번 실행되는 useEffect
+     *
+     * 페이지가 로드될 때 한 번 실행되며, 전체 페이지의 자식 요소 수를 계산하여 totalPageNumber 상태에 설정합니다. 
+     * 또한 윈도우의 리사이즈 이벤트를 처리하는 함수를 등록합니다.
+     ------------------------------------------------*/
     useEffect(() => {
         const wholePageElement = wholePageRef.current;
         // 전체 페이지의 자식 요소 수를 계산하여 설정
@@ -34,7 +39,11 @@ function useScrollNavigation() {
         };
     }, []);
 
-    // 스크롤 이벤트를 처리하는 함수
+    /**------------------------------------------------
+     * 스크롤 이벤트를 처리하는 함수
+     *
+     * 스크롤 이벤트를 처리하여 현재 스크롤 위치에 따라 현재 페이지를 업데이트합니다.
+     ------------------------------------------------*/
     const handleScroll = () => {
         for (let i = 1; i <= totalPageNumber; i++) {
             if (window.scrollY < currentInputs.currentWindowHeight * i) {
@@ -47,7 +56,11 @@ function useScrollNavigation() {
         }
     };
 
-    // 스크롤 이벤트 등록 및 해제
+    /**------------------------------------------------
+     * 스크롤 이벤트 등록 및 해제
+     *
+     * 스크롤 및 리사이징 이벤트에 대한 리스너를 등록하고, 컴포넌트가 언마운트될 때 이벤트 리스너를 해제합니다.
+     ------------------------------------------------*/
     useEffect(() => {
         window.addEventListener("scroll", handleScroll);
         return () => {
@@ -55,7 +68,11 @@ function useScrollNavigation() {
         };
     }, [currentInputs, totalPageNumber]);
 
-    // 마우스 휠 이벤트를 처리하는 함수
+    /**------------------------------------------------
+     * 마우스 휠 이벤트를 처리하는 함수
+     *
+     * 마우스 휠 이벤트를 처리하여 페이지를 위아래로 스크롤하도록 합니다.
+     ------------------------------------------------*/
     const handleWheel = (e) => {
         if (e.deltaY > 0) {
             let nextPage = currentInputs.currentPage + 1;
